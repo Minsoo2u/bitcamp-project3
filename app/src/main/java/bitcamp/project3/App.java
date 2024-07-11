62c0b969cb56ffb18dc55ad761156840317603e4
Bulgogi-Pizza <quitendexit@gmail.com>
2024-07-11 16:59:48 +0900

    feat: 기본 틀 구현

--- app/src/main/java/bitcamp/project3/App.java
+++ app/src/main/java/bitcamp/project3/App.java
@@ -1,17 +1,25 @@
 package bitcamp.project3;
 
 import bitcamp.project3.util.Print;
+import bitcamp.project3.util.PromptLibrary;
+import java.util.Stack;
 
 public class App {
 
-  static String[] mainMenus = {"대출 관리", "도서 관리", "사용자 관리", "종료"};
-  static String menuTitle = "메인 메뉴";
+  String[] mainMenus = {"대출 관리", "도서 관리", "사용자 관리", "종료"};
+  String menuTitle = "메인 메뉴";
+  Stack menuPath = new Stack();
 
   public static void main(String[] args) {
+    new App().execute();
+  }
+
+  void execute() {
     Print.printProgramTitle();
     Print.printTitle(menuTitle);
     Print.printMenus(mainMenus);
 
+    PromptLibrary.inputIntWithRange(0, mainMenus.length - 1, "%s>>", getMenuPath);
 
   }
 
